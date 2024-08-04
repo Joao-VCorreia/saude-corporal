86a1c7883b0d24663212e1d5c3c2e6e48bd28cc1
Joao-VCorreia <155659371+Joao-VCorreia@users.noreply.github.com>
2024-08-03 23:57:45 -0300

    Corrige codificação do modelo e aperfeiçoa banco de dados

--- scripts/gerar-dados-db-pacientes.py
+++ scripts/gerar-dados-db-pacientes.py
@@ -9,63 +9,89 @@ caminho_CSV = os.path.join(diretorio_Script, '../database/alunos-ativos-da-pos-g
 
 # Gerando dataframe
 df = pd.read_csv(caminho_CSV, sep=';', usecols=['nome_aluno']).sample(frac=1).reset_index(drop=True)
-df = df.rename(columns={'nome_aluno' : 'nome'})
+df = df.rename(columns={'nome_aluno' : 'NOME'})
+df['ID'] = range(1, len(df) + 1)
+df = df[['ID', 'NOME']]
 
 #Atribuindo sexo considerando nomes com maior frequencia
-# df_nomes_frequencia = df['nome'].map(lambda y: y.split()[0]).value_counts()
+# df_nomes_frequencia = df['NOME'].map(lambda y: y.split()[0]).value_counts()
 
 nomes_masculinos = ['Lucas', 'João', 'Gabriel', 'Matheus', 'Rafael', 'Guilherme', 'Gustavo', 'Bruno', 'Luiz', 'Paulo', 'Pedro', 'André', 'José', 'Rodrigo', 'Thiago', 'Felipe', 'Daniel', 'Marcelo', 'Eduardo']
 nomes_femininos = ['Ana', 'Maria', 'Mariana', 'Bruna', 'Gabriela', 'Camila', 'Fernanda', 'Amanda', 'Aline', 'Leticia', 'Larissa', 'Juliana', 'Jéssica', 'Beatriz', 'Marina']
 
-df['sexo'] = None
-df.loc[df['nome'].str.split().str[0].isin(nomes_masculinos), 'sexo'] = 'M'
-df.loc[df['nome'].str.split().str[0].isin(nomes_femininos), 'sexo'] = 'F'
+df['SEXO'] = None
+df.loc[df['NOME'].str.split().str[0].isin(nomes_masculinos), 'SEXO'] = 'M'
+df.loc[df['NOME'].str.split().str[0].isin(nomes_femininos), 'SEXO'] = 'F'
 
-lista_sexo_aleatorio = np.random.choice(['M', 'F'], size=df['sexo'].isnull().sum())
-df.loc[df['sexo'].isnull(), 'sexo'] = lista_sexo_aleatorio  
+lista_sexo_aleatorio = np.random.choice(['M', 'F'], size=df['SEXO'].isnull().sum())
+df.loc[df['SEXO'].isnull(), 'SEXO'] = lista_sexo_aleatorio  
 
 #Define data de nascimento e idade
-df['nascimento'] = None
-df.loc[df['nascimento'].isnull(), 'nascimento'] = np.random.choice(pd.date_range(start='1910-01-01', end='2019-12-31'), size=len(df))
-df['nascimento'] = pd.to_datetime(df['nascimento'])
+df['NASCIMENTO'] = None
+df.loc[df['NASCIMENTO'].isnull(), 'NASCIMENTO'] = np.random.choice(pd.date_range(start='1910-01-01', end='2019-12-31'), size=len(df))
+df['NASCIMENTO'] = pd.to_datetime(df['NASCIMENTO'])
 
-df['idade'] = (pd.Timestamp.now() - pd.to_datetime(df['nascimento'])).dt.days // 365
+df['IDADE'] = (pd.Timestamp.now() - pd.to_datetime(df['NASCIMENTO'])).dt.days // 365
 
 #define altura com influencia da idade e sexo
-df['altura'] = np.random.randint(120, 210, size=len(df))
-df.loc[df['idade'] <= 18, 'altura'] -= (25 * (1 - (df['idade'] / 18) ** 2 )).round() #quanto menor a idade, maior o decrescimo
-df.loc[df['idade'] >= 60, 'altura'] -= (30 * ((df['idade'] - 60) / (df['idade'].max() - 60)) ** 2).round() #quanto maior a idade, maior o decrescimo
+df['ALTURA'] = np.random.randint(120, 210, size=len(df))
+df.loc[df['IDADE'] <= 18, 'ALTURA'] -= (25 * (1 - (df['IDADE'] / 18) ** 2 )).round() #quanto menor a idade, maior o decrescimo
+df.loc[df['IDADE'] >= 60, 'ALTURA'] -= (30 * ((df['IDADE'] - 60) / (df['IDADE'].max() - 60)) ** 2).round() #quanto maior a idade, maior o decrescimo
 
-df.loc[df['sexo'] == 'F', 'altura'] -= np.random.randint(1, 8)
-df.loc[df['sexo'] == 'M', 'altura'] += np.random.randint(1, 8)
+df.loc[df['SEXO'] == 'F', 'ALTURA'] -= np.random.randint(1, 8)
+df.loc[df['SEXO'] == 'M', 'ALTURA'] += np.random.randint(1, 8)
 
 #define o peso com influencia da idade e sexo
-df['peso'] = np.round(np.random.uniform(25.0, 150, size=len(df)), decimals=2)
-df.loc[df['idade'] <= 18, 'peso'] -= (8 * (1 - (df['idade'] / 18) ** 2 )).round(decimals=2)
-df.loc[df['idade'] >= 60, 'peso'] -= (15 * ((df['idade'] - 60) / (df['idade'].max() - 60)) ** 2).round(decimals=2)
+df['PESO'] = np.round(np.random.uniform(25.0, 150, size=len(df)), decimals=2)
+df.loc[df['IDADE'] <= 18, 'PESO'] -= (8 * (1 - (df['IDADE'] / 18) ** 2 )).round(decimals=2)
+df.loc[df['IDADE'] >= 60, 'PESO'] -= (15 * ((df['IDADE'] - 60) / (df['IDADE'].max() - 60)) ** 2).round(decimals=2)
 
-df.loc[df['sexo'] == 'F', 'peso'] -= (df['peso'] * 0.05).round(decimals=2)
-df.loc[df['sexo'] == 'M', 'peso'] += (df['peso'] * 0.05).round(decimals=2)
+df.loc[df['SEXO'] == 'F', 'PESO'] -= (df['PESO'] * 0.05).round(decimals=2)
+df.loc[df['SEXO'] == 'M', 'PESO'] += (df['PESO'] * 0.05).round(decimals=2)
 
 # define pressao arterial
-df['pressao_arterial_sistolica'] = 10 * np.random.randint(5, 23, size=len(df))
-df['pressao_arterial_diastolica'] = 10 * np.random.randint(3, (df['pressao_arterial_sistolica']/10), size=len(df))
+df['PRESSAO_SISTOLICA'] = 10 * np.random.randint(5, 23, size=len(df))
+df['PRESSAO_DIASTOLICA'] = 10 * np.random.randint(3, (df['PRESSAO_SISTOLICA']/10), size=len(df))
 
-df.loc[df['idade'] <= 18, ['pressao_arterial_sistolica', 'pressao_arterial_diastolica']] -= (10 * np.random.randint(1, 4))
+df.loc[df['IDADE'] <= 18, ['PRESSAO_SISTOLICA', 'PRESSAO_DIASTOLICA']] -= (10 * np.random.randint(1, 4))
 
 # define glicemia com aumento na terceira idade
-df['glicemia'] = 5 * np.random.randint(8, 44, size=len(df))
-df.loc[df['idade'] >= 60, 'glicemia'] += 5 * np.random.randint(1, 4)
+df['GLICEMIA'] = 5 * np.random.randint(8, 44, size=len(df))
+df.loc[df['IDADE'] >= 60, 'GLICEMIA'] += 5 * np.random.randint(1, 4)
 
 # Gera um valor de frequencia cardiaca com influencia da idade
-df['frequencia_cardiaca'] = np.random.randint(30, 210, size=len(df))
+df['FREQUENCIA_CARDIACA'] = np.random.randint(30, 210, size=len(df))
 
-df.loc[df['idade'] <= 18, 'frequencia_cardiaca'] += (np.random.randint(20, 40) * (1 - (df['idade'] / 18) ** 2 )).round()
-df.loc[df['idade'] >= 60, 'frequencia_cardiaca'] += (np.random.randint(10, 30) * ((df['idade'] - 60) / (df['idade'].max() - 60)) ** 2).round()
+df.loc[df['IDADE'] <= 18, 'FREQUENCIA_CARDIACA'] += (np.random.randint(20, 40) * (1 - (df['IDADE'] / 18) ** 2 )).round()
+df.loc[df['IDADE'] >= 60, 'FREQUENCIA_CARDIACA'] += (np.random.randint(10, 30) * ((df['IDADE'] - 60) / (df['IDADE'].max() - 60)) ** 2).round()
+
+print(df)
 
 caminho_Database = os.path.join(diretorio_Script, '../mysite/db.sqlite3')
+
 with sqlite3.connect(caminho_Database) as conexao:
-    df.to_sql('HealthHub_pacientes', conexao, if_exists='replace', index=False)
+
+    cursor = conexao.cursor()
+
+    cursor.execute('DROP TABLE IF EXISTS HealthHub_pacientes')
+
+    cursor.execute('''
+        CREATE TABLE HealthHub_pacientes (
+            ID INTEGER PRIMARY KEY NOT NULL,
+            NOME TEXT NOT NULL,
+            SEXO TEXT NOT NULL,
+            NASCIMENTO DATE NOT NULL,
+            IDADE INTEGER NOT NULL,
+            ALTURA INTEGER NOT NULL,
+            PESO REAL NOT NULL,
+            PRESSAO_SISTOLICA INTEGER NOT NULL,
+            PRESSAO_DIASTOLICA INTEGER NOT NULL,
+            GLICEMIA INTEGER NOT NULL,
+            FREQUENCIA_CARDIACA INTEGER NOT NULL
+        )
+    ''')
+
+    df.to_sql('HealthHub_pacientes', conexao, if_exists='append', index=False)
 
 """
 df['circunferencia-cintura']
